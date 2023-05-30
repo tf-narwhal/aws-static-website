0120717cd17fa016a53e066e2bed0c8d3e574146
Ryan Flynn <ryan@narwh.al>
2023-05-30 15:44:39 -0700

    allow for external arns to connect to bucket

--- s3.tf
+++ s3.tf
@@ -10,26 +10,60 @@ module "s3_static_website" {
 # Set the S3 bucket policy to allow access from the CloudFront distribution
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = module.s3_static_website.s3_bucket_id
-  policy = jsonencode({
-    "Version": "2008-10-17",
-    "Id": "PolicyForCloudFrontPrivateContent",
-    "Statement": [
-      {
-        "Sid": "AllowCloudFrontServicePrincipal",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "cloudfront.amazonaws.com"
-        },
-        "Action": "s3:GetObject",
-        "Resource": [
-          "${module.s3_static_website.s3_bucket_arn}/*"
-        ],
-        "Condition": {
-          "StringEquals": {
-            "AWS:SourceArn": "${module.cloudfront.cloudfront_distribution_arn}"
-          }
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  dynamic "statement" {
+    for_each = concat(
+      [{
+        sid = "AllowCloudFrontServicePrincipal"
+        principals = {
+          type        = "Service"
+          identifiers = ["cloudfront.amazonaws.com"]
+        }
+        actions = ["s3:GetObject"]
+        resources = ["${module.s3_static_website.s3_bucket_arn}/*", "${module.s3_static_website.s3_bucket_arn}"]
+        condition = [{
+          test     = "StringEquals"
+          variable = "AWS:SourceArn"
+          values   = [module.cloudfront.cloudfront_distribution_arn]
+        }]
+      }],
+      [for arn in var.role_arns : {
+        sid = "AllowRoleAccess"
+        principals = {
+          type        = "AWS"
+          identifiers = [arn]
+        }
+        actions   = ["s3:*"]
+        resources = ["${module.s3_static_website.s3_bucket_arn}/*", "${module.s3_static_website.s3_bucket_arn}"]
+        condition = []
+      }]
+    )
+
+    content {
+      sid       = statement.value.sid
+      effect    = "Allow"
+      actions   = statement.value.actions
+      resources = statement.value.resources
+
+      principals {
+        type        = statement.value.principals.type
+        identifiers = statement.value.principals.identifiers
+      }
+
+      dynamic "condition" {
+        for_each = statement.value.condition
+        content {
+          test     = condition.value.test
+          variable = condition.value.variable
+          values   = condition.value.values
         }
       }
-    ]
-  })
+    }
+  }
 }
+
+
+
--- variables.tf
+++ variables.tf
@@ -12,3 +12,9 @@ variable "description" {
   type        = string
   description = "The description to be used where applicable"
 }
+
+variable "role_arns" {
+  description = "List of ARNs for the roles to which you want to grant access"
+  type        = list(string)
+  default     = []
+}
